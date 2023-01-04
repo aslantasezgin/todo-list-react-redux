819a8a0d0635c2f038bbff6d573a9eb0d000d918
John Doe <aslantasezginn@gmail.com>
2023-01-04 21:00:19 +0300

    todo added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import './App.css';
 import Header from './components/Header/Header.component';
 import Input from './components/Input/Input.component';
 import ListContent from './components/ListContent/ListContent.component';
+import { useSelector } from 'react-redux';
+
+
+
 
 function App() {
+
+  const {todoList,todoFilter}=useSelector(state => state.todo)
+  console.log(todoList,todoFilter)
+
   return (
     <div className="md:container md:mx-auto">
     <Header>
--- src/components/Input/Input.component.jsx
+++ src/components/Input/Input.component.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import './Input.styles.css'
+import {addTodo} from '../../store/actions/todoActions'
 
 const Input = () => {
+
+    const [text,setText] = useState('')
+    const dispatch = useDispatch()
+
     return(
         <div className="input-wrapper">
-            <input autoFocus placeholder="Create New Todo"></input>
+            <input value={text} onChange={(e) => {setText(e.target.value)}} 
+            onKeyPress={(e) => {
+               if(e.key==="Enter" && text.trim()!==""){
+                dispatch(addTodo({id:new Date().getTime(),text:text,complete:false}))
+                setText('')
+               }
+            }}
+            autoFocus placeholder="Create New Todo"></input>
         </div>
     )
 }
